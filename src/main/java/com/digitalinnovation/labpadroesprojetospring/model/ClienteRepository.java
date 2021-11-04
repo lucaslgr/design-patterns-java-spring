8d2cd6f98382d3de5b28c72b6d71dbb40e7d558d
lucaslgr <lucaslgr1206@gmail.com>
2021-11-04 09:01:20 -0300

    first commit

---
+++ src/main/java/com/digitalinnovation/labpadroesprojetospring/model/ClienteRepository.java
@@ -0,0 +1,6 @@
+package com.digitalinnovation.labpadroesprojetospring.model;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface ClienteRepository extends CrudRepository<Cliente, Long> {
+}
---
+++ src/main/java/com/digitalinnovation/labpadroesprojetospring/model/EnderecoRepository.java
@@ -0,0 +1,6 @@
+package com.digitalinnovation.labpadroesprojetospring.model;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface EnderecoRepository extends CrudRepository<Endereco, String> {
+}
